bd986fe4c412c29fa686486f57baea4cb11ccc36
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-08-04 13:06:03 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
+
+{
+  "overrides": [
+    {
+      "files": ["tests/**/*"],
+      "env": {
+        "jest": true
+      }
+    }
+  ]
+}
